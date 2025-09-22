51a897afb6a8e401190930a560321b43d8a5fe09
oskar <oskar.vistorskis@gmail.com>
2025-09-22 09:41:35 +0300

    Added ReedMullerEncoder

---
+++ ReedMullerEncoder/ReedMullerEncoder.cs
@@ -0,0 +1,19 @@
+﻿using CodingTheory.Math;
+
+namespace CodingTheory.ReedMuller;
+
+public class ReedMullerEncoder
+{
+    private Matrix m_generatorMatrix;
+
+    // (1, m) Reed-Muller code
+    public ReedMullerEncoder(int m)
+    {
+        if (m < 2)
+            throw new ArgumentException("Parameter m must be at least 2.", nameof(m));
+
+        m_generatorMatrix = GeneratorMatrixBuilder.Build(m);
+    }
+
+    public Vector Encode(Vector message) => message * m_generatorMatrix;
+}
