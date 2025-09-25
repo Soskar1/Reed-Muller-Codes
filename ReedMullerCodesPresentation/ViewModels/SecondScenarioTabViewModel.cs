746d9c14b3ab0ae78043dc47cbb0080f529f3810
oskar <oskar.vistorskis@gmail.com>
2025-09-25 21:13:19 +0300

    Implemented first scenario

---
+++ ReedMullerCodesPresentation/ViewModels/SecondScenarioTabViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CodingTheory.Presentation;
+
+public class SecondScenarioTabViewModel : TabViewModelBase
+{
+    public SecondScenarioTabViewModel() : base("2nd Scenario")
+    {
+
+    }
+}
--- ReedMullerEncoder/ReedMullerEncoder.cs
+++ ReedMullerEncoder/ReedMullerEncoder.cs
@@ -5,6 +5,7 @@ namespace CodingTheory.ReedMuller;
 public class ReedMullerEncoder
 {
     private readonly MatrixMod2 m_generatorMatrix;
+    public int RequiredMessageLength { get; init; }
 
     // (1, m) Reed-Muller code
     public ReedMullerEncoder(int m)
@@ -12,8 +13,15 @@ public class ReedMullerEncoder
         if (m < 2)
             throw new ArgumentException("Parameter m must be at least 2.", nameof(m));
 
+        RequiredMessageLength = m + 1;
         m_generatorMatrix = GeneratorMatrixBuilder.Build(m);
     }
 
-    public Vector Encode(Vector message) => message * m_generatorMatrix;
+    public Vector Encode(Vector message)
+    {
+        if (message.Length != RequiredMessageLength)
+            throw new ArgumentException("Invalid vector length");
+        
+        return message * m_generatorMatrix;
+    }
 }
