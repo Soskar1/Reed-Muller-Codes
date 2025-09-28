674a936453b327bda4b3e52de992f97727cb05e3
oskar <oskar.vistorskis@gmail.com>
2025-09-28 16:08:51 +0300

    Added 24 bit bmp file distortion

--- Channel/Channel.cs
+++ Channel/Channel.cs
@@ -11,7 +11,7 @@ public class Channel
     {
         if (errorProbability < 0.0 || errorProbability > 1.0)
             throw new ArgumentException("Error probability must be between 0 and 1.", nameof(errorProbability));
-        
+
         m_errorProbability = errorProbability;
         m_random = new Random();
     }
@@ -20,7 +20,7 @@ public class Channel
     {
         int[] transmittedData = new int[data.Length];
 
-        for (int i = 0; i< data.Length; ++i)
+        for (int i = 0; i < data.Length; ++i)
         {
             if (m_random.NextDouble() < m_errorProbability)
                 transmittedData[i] = data[i] ^ 1;
@@ -29,5 +29,19 @@ public class Channel
         }
 
         return new Vector(transmittedData);
+    }
+
+    public byte[] PassThrough(byte[] data)
+    {
+        byte[] distorted = new byte[data.Length];
+
+        for (int i = 0; i < data.Length; ++i)
+        {
+            Vector v = Vector.ByteToVector(data[i]);
+            v = PassThrough(v);
+            distorted[i] = (byte)v;
+        }
+
+        return distorted;
     }
 }
--- ThirdScenario/MainWindow.xaml.cs
+++ ThirdScenario/MainWindow.xaml.cs
@@ -1,9 +1,21 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using CodingTheory.Channels;
+using System.Diagnostics;
 
 namespace CodingTheory.Presentation;
 
 public partial class MainWindow : Window
 {
+    private Channel m_channel;
+    private string m_selectedFile;
+    private const ushort m_bmpFileType = 0x4D42;
+    private const ushort m_bmpBitsPerPixel = 24;
+    private const int m_fileHeaderSize = 14;
+    private const int m_infoHeaderSize = 40;
+
     public MainWindow()
     {
         InitializeComponent();
@@ -11,6 +23,68 @@ public partial class MainWindow : Window
 
     private void SelectImageClick(object sender, RoutedEventArgs e)
     {
+        OpenFileDialog fileDialog = new OpenFileDialog();
+        fileDialog.Filter = "bmp24 Files | *.bmp";
+        
+        bool? success = fileDialog.ShowDialog();
+        if (success == true)
+        {
+            fileNameLabel.Content = fileDialog.SafeFileName;
+            m_selectedFile = fileDialog.FileName;
 
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(fileDialog.FileName);
+            bitmap.EndInit();
+
+            imageInput.Source = bitmap;
+        }
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        m_channel = new Channel(float.Parse(probabilityInput.Text));
+
+        using (FileStream fs = new FileStream(m_selectedFile, FileMode.Open, FileAccess.Read))
+        using (BinaryReader reader = new BinaryReader(fs))
+        {
+            byte[] fileHeader = reader.ReadBytes(m_fileHeaderSize);
+            byte[] infoHeader = reader.ReadBytes(m_infoHeaderSize);
+
+            byte[] GetTypeBytes() => [fileHeader[0], fileHeader[1]];
+            byte[] GetBitsPerPixelBytes() => [infoHeader[14], infoHeader[15]];
+            byte[] GetOffsetBytes() => [fileHeader[10], fileHeader[11], fileHeader[12], fileHeader[13]];
+
+            ushort type = BitConverter.ToUInt16(GetTypeBytes(), 0);  
+            if (type != m_bmpFileType)
+            {
+                errorDisplayLabel.Content = "Not a BMP file";
+                return;
+            }
+
+            ushort bitCount = BitConverter.ToUInt16(GetBitsPerPixelBytes(), 0);
+            if (bitCount != m_bmpBitsPerPixel)
+            {
+                errorDisplayLabel.Content = "Not a 24-bit BMP";
+                return;
+            }
+
+            uint offset = BitConverter.ToUInt32(GetOffsetBytes(), 0);
+            byte[] additionalHeaderData = reader.ReadBytes((int)offset - m_fileHeaderSize - m_infoHeaderSize);
+
+            fs.Seek(offset, SeekOrigin.Begin);
+            byte[] pixels = reader.ReadBytes((int)(fs.Length - offset));
+
+            pixels = m_channel.PassThrough(pixels);
+            byte[] bmpFileData = fileHeader.Concat(infoHeader).Concat(additionalHeaderData).Concat(pixels).ToArray();
+
+            MemoryStream memoryStream = new MemoryStream(bmpFileData);
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.StreamSource = memoryStream;
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.EndInit();
+            withoutAlgorithmImage.Source = bitmap;
+        }
     }
 }
