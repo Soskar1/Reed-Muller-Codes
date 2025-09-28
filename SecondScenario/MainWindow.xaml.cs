09cb7e8b9d7b0ac0ac665366a586dda9acb1b1b7
oskar <oskar.vistorskis@gmail.com>
2025-09-28 11:41:15 +0300

    Implemented 2nd scenario

--- SecondScenario/MainWindow.xaml.cs
+++ SecondScenario/MainWindow.xaml.cs
@@ -1,24 +1,51 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Vector = CodingTheory.Math.Vector;
+using Channel = CodingTheory.Channels.Channel;
+using System.Text;
+using CodingTheory.ReedMuller;
 
-namespace SecondScenario
+namespace CodingTheory.Presentation;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private Channel m_channel;
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void showResultButton_Click(object sender, RoutedEventArgs e)
+    {
+        m_channel = new Channel(float.Parse(probabilityInput.Text));
+        PassRawTextThroughChannel(textInput.Text);
+        EncodeText(textInput.Text);
+    }
+
+    private void PassRawTextThroughChannel(string text)
+    {
+        List<byte> distortedRawText = new List<byte>();
+        foreach (char c in text)
         {
-            InitializeComponent();
+            Vector v = Vector.ByteToVector((byte)c);
+            v = m_channel.PassThrough(v);
+            distortedRawText.Add((byte)v);
         }
+        withoutAlgorithmTextBox.Text = Encoding.UTF8.GetString(distortedRawText.ToArray());
+    }
+
+    private void EncodeText(string text)
+    {
+        ReedMullerEncoder encoder = new ReedMullerEncoder(byte.Parse(mInput.Text));
+        byte[] bytes = Encoding.UTF8.GetBytes(text);
+        List<Vector> encodedMessage = encoder.Encode(bytes);
+
+        for (int i = 2; i < encodedMessage.Count; ++i)
+            encodedMessage[i] = m_channel.PassThrough(encodedMessage[i]);
+
+        ReedMullerDecoder decoder = new ReedMullerDecoder();
+        bytes = decoder.Decode(encodedMessage.ToArray());
+
+        withAlgorithmTextBox.Text = Encoding.UTF8.GetString(bytes);
     }
 }
